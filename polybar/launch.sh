9ffd0cce0f0118706fd1b71bea0095ba40c21731
ArthurD3nt <francescomazzucco01@gmail.com>
2022-10-05 16:33:42 +0200

    Fixed some configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,10 +11,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
+    MONITOR=$m polybar mybar &
   done
 else
-  polybar --reload mybar &
+  polybar mybar &
 fi
 
 echo "Polybar launched..."
